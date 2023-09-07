102f195ff4c9b082fe9f0b6ca12a2040c6062c72
agostinhosamil <agostinhosaml832@gmail.com>
2023-09-07 09:35:34 +0100

    feat(module:config/extension): correct parse config

--- php-module/config/modules.php
+++ php-module/config/modules.php
@@ -6,7 +6,7 @@ $module->exports = [
   /**
    * php module json parser
    */
-  'module:parser .json' => function (string $fileAbsolutePath) {
+  'module:parse .json' => function (string $fileAbsolutePath) {
     # Make sure the given file name reference
     # is a valid reference for a file containing
     # some information that should be used any how
